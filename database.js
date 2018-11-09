5a94eaeae58e7efc46951659e6faa768b1c9ca8b
Sang-Joon Song <saso7680@colorado.edu>
2018-11-09 12:39:50 -0700

    q

--- database.js
+++ database.js
@@ -1,13 +1,6 @@
 var pgp = require('pg-promise')();
 
-const dbConfig = {
-   host: 'localhost',
-   port: 5432,
-   database: 'lab6',
-   user: 'postgres',
-   password: '' // Fill in your PostgreSQL password here. Use empty string
-                // if you did not set a password
-};
+
 var dbConfig = process.env.DATABASE_URL;
 var db = pgp(dbConfig);
 
